8fdf1a2281e738dc14a22db31fc84c9c350de7e2
Takata1128 <t1710357@edu.cc.uec.ac.jp>
2022-03-07 15:09:27 +0900

    とちゅう

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use clap::Parser;
+use std::collections::HashSet;
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::path::Path;
 
 #[derive(Parser, Debug)]
 #[clap(
-    name = "RPN program",
+    name = "pokemon_wordle support program",
     version = "1.0.0",
     author = "rokah",
-    about = "RPN calculator."
+    about = "Support to solve pokemon wordle."
 )]
 struct Opts {
     /// Sets the level of verbosity
@@ -56,10 +58,67 @@ pub fn write<P: AsRef<Path>>(path: P, v: Vec<String>) -> std::io::Result<()> {
 fn main() {
     let opts = Opts::parse();
 
+    let filename: &str = "./pokemon_names.txt";
+
     if opts.scrape {
         let result = Scraper::scrape("https://wiki.xn--rckteqa2e.com/wiki/%E3%83%9D%E3%82%B1%E3%83%A2%E3%83%B3%E4%B8%80%E8%A6%A7","table.bluetable tbody tr td:nth-child(2) > a");
         println!("{:?}", result);
-        let result = write("./pokemon_names.txt", result.unwrap());
+        let result = write(filename, result.unwrap());
         println!("{:?}", result);
+    }
+
+    {
+        let mut f = File::open(filename).expect("file not found.");
+
+        let mut contents = String::new();
+        f.read_to_string(&mut contents)
+            .expect("something went wrong reading the file.");
+
+        let v: Vec<&str> = (&contents).split('\n').collect();
+
+        println!("{:?}", v);
+
+        let mut chars: HashSet<char> = HashSet::new();
+
+        for pokemon in v.into_iter() {
+            for c in pokemon.chars() {
+                chars.insert(c);
+            }
+        }
+        let mut coodinates = vec![chars; 5];
+
+        loop {
+            // ポケモン名入力
+
+            let mut pokemon_name = String::new();
+            io::stdin()
+                .read_line(&mut pokemon_name)
+                .expect("Failed to read line.");
+
+            // チェック文字列入力
+            // 正解なら終了
+
+            // . miss
+            // # near
+            // * hit
+
+            let mut check = String::new();
+            io::stdin()
+                .read_line(&mut check)
+                .expect("Failed to read line.");
+
+            for (a, (b, c)) in check.chars().zip(pokemon_name.chars()).enumerate() {
+                if b == '.' {
+                    // すべての箇所の候補から該当文字を除外
+                } else if b == '#' {
+                    // 該当箇所の候補から該当文字を除外
+                } else if b == '*' {
+                    // 該当箇所の候補を決定
+                }
+                println!("{} {}", a, b);
+            }
+
+            // 候補の条件を満たすポケモンを表示
+        }
     }
 }
